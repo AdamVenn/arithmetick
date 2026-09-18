a30c21d3f91bab1fd001a9901a99ca64ff18801b
AdamVenn <46652493+AdamVenn@users.noreply.github.com>
2026-09-18 10:53:11 -0400

    Formatting

--- compile_time_math.hpp
+++ compile_time_math.hpp
@@ -9,24 +9,28 @@ namespace compile_time_math
 #ifdef __cpp_lib_saturation_arithmetic
 using std::saturate_cast;
 #else
-template<std::integral Res, std::integral Tp>
+template <std::integral Res, std::integral Tp>
 constexpr Res saturate_cast(Tp x) noexcept
 {
-    if constexpr (std::is_signed_v<Res> == std::is_signed_v<Tp>) {
-        if constexpr (std::numeric_limits<Res>::digits < std::numeric_limits<Tp>::digits) {
+    if constexpr (std::is_signed_v<Res> == std::is_signed_v<Tp>)
+    {
+        if constexpr (std::numeric_limits<Res>::digits < std::numeric_limits<Tp>::digits)
+        {
             if (x < std::numeric_limits<Res>::min())
                 return std::numeric_limits<Res>::min();
             if (x > std::numeric_limits<Res>::max())
                 return std::numeric_limits<Res>::max();
         }
     }
-    else if constexpr (std::is_signed_v<Tp>) {
+    else if constexpr (std::is_signed_v<Tp>)
+    {
         if (x < 0)
             return 0;
         if (static_cast<std::make_unsigned_t<Tp>>(x) > std::numeric_limits<Res>::max())
             return std::numeric_limits<Res>::max();
     }
-    else {
+    else
+    {
         if (x > static_cast<std::make_unsigned_t<Res>>(std::numeric_limits<Res>::max()))
             return std::numeric_limits<Res>::max();
     }
